1ba8738051afa19f591875bc16d5ecbbb2926d1d
just1ce2194 <just1ce2194@gmail.com>
2015-03-15 14:13:11 +0200

    sdf
    sdf

---
+++ MainModule/src/main.java
@@ -0,0 +1,41 @@
+/**
+ * Created by just1ce on 15.03.2015.
+ */
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.io.FileInputStream;
+import java.io.*;
+
+
+
+public class main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+
+
+    }
+
+   //dfdsfgsdfsdfsdfsdfsd
+    @Override
+    public void start(Stage primaryStage) throws FileNotFoundException, IOException {
+        String line = null;
+        FileReader fileReader =new FileReader("E:/test.txt");
+        BufferedReader bufferedReader =
+                new BufferedReader(fileReader);
+
+        while((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        // Always close files.
+        bufferedReader.close();
+    }
+
+}
